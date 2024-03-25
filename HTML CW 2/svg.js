e6af5b5a3b2aaed1a6f7a01b3f96a42bc9d9eb7e
Nilesh shanilka <nilesh.20221190@iit.ac.lk>
2024-03-25 10:36:17 +0530

    Initial commit for web cw

---
+++ HTML CW 2/svg.js
@@ -0,0 +1,42 @@
+
+document.getElementById("group1").
+addEventListener("click", function() {
+	document.location.href="index.html";
+});
+
+document.getElementById("group3").
+addEventListener("click", function() {
+	document.location.href="schedule.html";
+});
+
+document.getElementById("group4").
+addEventListener("click", function() {
+	document.location.href="Home/home.html";
+});
+
+document.getElementById("group5").
+addEventListener("click", function() {
+	document.location.href="schedule.html";
+});
+
+document.getElementById("group6").
+addEventListener("click", function() {
+	document.location.href="schedule.html";
+});
+
+document.getElementById("group7").
+addEventListener("click", function() {
+	document.location.href="imagegallery.html";
+});
+
+document.getElementById("group8").
+addEventListener("click", function() {
+	document.location.href="svg.html";
+});
+
+document.getElementById("group9").
+addEventListener("click", function() {
+	document.location.href="gallery.html";
+});
+			
+		
